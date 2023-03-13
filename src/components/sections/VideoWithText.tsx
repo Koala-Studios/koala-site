c04b0f3a34dd0470d2d418236d25dbfb2b5ad912
frankev6 <frank.ev6@gmail.com>
2023-03-12 22:05:33 -0400

    video replay on intersection

--- src/components/sections/VideoWithText.tsx
+++ src/components/sections/VideoWithText.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "../../styles/Sections.module.css";
 interface Props {
   video_placement: "left" | "right";
@@ -14,12 +14,41 @@ interface Props {
   loop?: boolean;
 }
 
-const ImageWithText: React.FC<Props> = (props) => {
+const observer = new IntersectionObserver(
+  (entries) => {
+    console.log("HELOLDAFK");
+    entries.forEach((entry: any) => {
+      if (!entry.isIntersecting) {
+        entry.target.pause();
+        entry.target.currentTime = 0;
+        //   playState = false;
+      } else {
+        entry.target.play();
+        //   playState = true;
+      }
+    });
+  },
+  { rootMargin: "-15% 0% -15% 0%" }
+);
+
+const VideoWithText: React.FC<Props> = (props) => {
+  const videoRef = useRef<any>();
+
+  useEffect(() => {
+    if (videoRef) {
+      observer.observe(videoRef.current!);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <section
       style={props.styles}
       className={`${styles.image_with_text_container} ${
-        props.video_placement == "left" ? styles.left : ""
+        props.video_placement === "left" ? styles.left : ""
       }`}
     >
       <div className={styles.image_with_text_info}>
@@ -28,6 +57,7 @@ const ImageWithText: React.FC<Props> = (props) => {
         {props.children}
       </div>
       <video
+        ref={videoRef}
         style={props.videoStyles}
         autoPlay
         loop={props.loop}
@@ -35,9 +65,9 @@ const ImageWithText: React.FC<Props> = (props) => {
         width={props.width}
         height={props.height}
       >
-        <source src={props.src}  />
+        <source src={props.src} />
       </video>
     </section>
   );
 };
-export default ImageWithText;
+export default VideoWithText;
--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -5,12 +5,11 @@ import TitleWithText from "../../components/sections/TitleWithText";
 import AboutpageHero from "./AboutpageHero";
 import ScrollingText from "../../components/sections/ScrollingText";
 import { hover } from "@testing-library/user-event/dist/hover";
-import styles from "../../styles/Navbar.module.css";
 import Footer from "../../components/sections/Footer";
 import { Link } from "react-router-dom";
 const AlloPage = () => {
   return (
-    <div style={{ backgroundColor:"#181A17", overflowX:'hidden'}}>
+    <div style={{ backgroundColor: "#181A17", overflowX: "hidden" }}>
       <AboutpageHero></AboutpageHero>
       <ScrollingText
         TextArray={[
@@ -23,12 +22,6 @@ const AlloPage = () => {
         ]}
       />
 
-        <img className={styles.logo} style={{marginTop:150  }}
-          alt="Koala Studios Logo"
-          src="/images/koala_logo_white.png"
-          width={260}
-        />
-
       <ImageWithText
         image_placement={"right"}
         src={"../images/project/allo/vanilla_noncreamer.webp"}
@@ -38,36 +31,56 @@ const AlloPage = () => {
         width={""}
         height={""}
         imgStyles={{ borderRadius: "1.5rem" }}
-        > 
-        <p style={{fontFamily:'Poppins-Regular'}}>
-        Our mission is to help brands succeed online by providing high-quality services that align with their values and goals. 
-            At Koala Studios, we prioritize quality above all else.
-            We understand that every brand is unique, and that's why we take the time to get to know our clients and their values.
-            <br/><br/>We believe that a website should not only be visually appealing but also reflect the brand's personality and message.
-            That's why we work closely with our clients to create custom solutions that align with their brand's identity.
-            <br/><br/>Thank you for considering Koala Studios for your needs. We look forward to helping you take your brand to the next level!"
+      >
+        <p style={{ fontFamily: "Poppins-Regular" }}>
+          Our mission is to help brands succeed online by providing high-quality
+          services that align with their values and goals. At Koala Studios, we
+          prioritize quality above all else. We understand that every brand is
+          unique, and that's why we take the time to get to know our clients and
+          their values.
+          <br />
+          <br />
+          We believe that a website should not only be visually appealing but
+          also reflect the brand's personality and message. That's why we work
+          closely with our clients to create custom solutions that align with
+          their brand's identity.
+          <br />
+          <br />
+          Thank you for considering Koala Studios for your needs. We look
+          forward to helping you take your brand to the next level!"
         </p>
       </ImageWithText>
 
-      <div style={{padding: "var(--global-padding)", color: "#ede7e0", display:'flex', gap:10, flexDirection:'column', alignItems:'center', marginBottom:150, marginTop:100}}>
-        <h2>Want to learn more about us?</h2>
-        <p>Take a look through our projects!</p>
+      <div
+        style={{
+          padding: "var(--global-padding)",
+          color: "#282420",
+          background: "#ede7e0",
+          display: "flex",
+          gap: 20,
+          flexDirection: "column",
+          alignItems: "center",
+          paddingBottom: 100,
+          paddingTop: 100,
+        }}
+      >
+        <h2 style={{ margin: 0 }}>Want to learn more about us?</h2>
+        <p style={{ margin: 0 }}>Take a look through our projects!</p>
         <Link to="/projects">
-            <Button
-              style={{
-                backgroundColor: "#39744f",
-                whiteSpace: "nowrap",
-                border: "2px solid #39744f",
-              }}
-              type={"primary"}
-            >
-              View Our Work
-            </Button>
-          </Link>
+          <Button
+            style={{
+              backgroundColor: "#39744f",
+              whiteSpace: "nowrap",
+              border: "2px solid #39744f",
+            }}
+            type={"primary"}
+          >
+            View Our Work
+          </Button>
+        </Link>
       </div>
-    <Footer></Footer>
+      <Footer></Footer>
     </div>
-    
   );
 };
 
