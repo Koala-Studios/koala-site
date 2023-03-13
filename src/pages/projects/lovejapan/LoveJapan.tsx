96c2a8342cd2ace5688cdab073523ff6653069f0
danveloso <danielvelo11@gmail.com>
2023-03-13 16:41:44 -0400

    lovejapan changes

--- src/pages/projects/lovejapan/LoveJapan.tsx
+++ src/pages/projects/lovejapan/LoveJapan.tsx
@@ -8,6 +8,7 @@ import ProjectHero from "../../../components/sections/ProjectHero";
 import ScrollingText from "../../../components/sections/ScrollingText";
 import TitleWithText from "../../../components/sections/TitleWithText";
 import styles from "../../../styles/ProjectSections.module.css";
+import StatBadge from "../StatBadge";
 
 const LoveJapan = () => {
   console.log(project);
@@ -20,10 +21,10 @@ const LoveJapan = () => {
         TextArray={[
             "UI/UX Design",
             "Consulting",
-            "UI/UX Design",
+            "Data Analysis",
             "Consulting",
             "UI/UX Design",
-            "Consulting",
+            "Data Analysis",
         ]}
       />
       <ImageWithText
@@ -34,17 +35,17 @@ const LoveJapan = () => {
         text={''}
         width={"40%"}
         height={""}
-        imgStyles={{ borderRadius: "1.5rem" }}
+        imgStyles={{ borderRadius: "1.5rem", border:'2px solid #ede7e0' }}
       >
-        <p>The store owner was inspired to create a store after falling in love with Japan during a trip over three years ago.
+        <p>The client was inspired to create a store after falling in love with Japan during a trip over three years ago.
         They were captivated by the beauty of the country, its people, culture, and delicious food. Upon returning to North America,
         they realized that there were many others who shared the same love for Japan. 
-        The owner's mission is to provide the best and most authentic Japanese-themed products to spread the joy of Japan around the world, 
+        LoveJapan4Life's mission is to provide the best and most authentic Japanese-themed products to spread the joy of Japan around the world, 
         even to those who cannot travel there.</p>
       </ImageWithText>
         <TitleWithText orientation={"horizontal"} title={"How did we help?"} text={''}>
-
-        <p>
+        <div style={{display:'flex', width:'100%', justifyContent:'center', textAlign:'left'}}>
+        <p style={{maxWidth:600}}>
             After conducting a thorough analysis, we were able to pinpoint the most pressing issues and devised a comprehensive plan to address them.
             Leveraging insights from our analytics data, we identified the key bottlenecks in the customer conversion process and formulated targeted solutions to improve the client's website. 
             By working closely with the client, we provided a clear roadmap for implementing these changes, which led to a significant boost in performance.<br/><br/>
@@ -52,8 +53,51 @@ const LoveJapan = () => {
             These included increased website traffic, higher engagement rates, and a substantial improvement in overall customer satisfaction.
             By leveraging data-driven insights and providing actionable recommendations, we were able to help the client achieve their goals and drive meaningful growth
         </p>
+        </div>
             </TitleWithText>
 
+            <h2 style={{ color: "#ede7e0", fontSize: "2rem" }}> Our Results</h2>
+
+<div
+  style={{
+    padding: "var(--global-padding)",
+    display: "flex",
+    gap: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#ede7e0",
+    fontSize: 13,
+    marginTop: 50,
+    marginBottom:200
+  }}
+>
+    <StatBadge
+      LargeText="+159%"
+      SmallText="Conversion Rate"
+      UnderText={
+        <div>
+        From 0.8%
+        </div>
+      }
+    />
+    <StatBadge
+      LargeText="+216% "
+      SmallText="Sessions"
+      UnderText={
+        <div>
+        From +100,000 Sessions
+        </div>
+      }
+    />
+    <StatBadge
+      LargeText="+652%"
+      SmallText="Revenue"
+    />
+    <StatBadge
+      LargeText="16"
+      SmallText="Ramens Served"
+    />
+</div>
 
       <NextProject next_link={`/projects/${project.next_handle}`} />
       <Footer></Footer>
