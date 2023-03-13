93be074c6006b8ed837d2aadd0b3c75f096ef7e6
frankev6 <frank.ev6@gmail.com>
2023-03-13 16:34:36 -0400

    about

---
+++ src/components/sections/GradientHero.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../../components/Button";
+import styles from "../../styles/Home.module.css";
+
+interface Props {
+  gradient: string;
+  title: string;
+  height?: string;
+}
+
+const GradientHero: React.FC<Props> = (props) => {
+  return (
+    <div style={{ height: props.height }} className={styles.hero_section}>
+      <section
+        style={{ height: props.height }}
+        className={styles.hero_container}
+      >
+        <div
+          style={{
+            // background: "linear-gradient(45deg, #181a17, #203f44,#433960)",
+            background: props.gradient,
+          }}
+          className={styles.hero_video}
+        ></div>
+        <div className={styles.hero_content}>
+          <h1 className={styles.title}>{props.title}</h1>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default GradientHero;
--- src/pages/services/ServicesSection.tsx
+++ src/pages/services/ServicesSection.tsx
@@ -1,63 +1,71 @@
 import React from "react";
 import Button from "../../components/Button";
 import Footer from "../../components/sections/Footer";
+import GradientHero from "../../components/sections/GradientHero";
 import ImageWithText from "../../components/sections/ImageWithText";
+import ScrollingText from "../../components/sections/ScrollingText";
 import StandaloneImage from "../../components/sections/StandaloneImage";
 import TitleWithText from "../../components/sections/TitleWithText";
 import styles from "../../styles/Home.module.css";
 
 const ServicesSection = () => {
   return (
-      <div>
-        <h2 style={{marginTop:'7rem', color:"#ede7e0", fontSize:'2.5rem'}}> Our Services </h2>
-        <ImageWithText
-          image_placement="left"
-          src="/images/placeholder.png"
-          alt="TODO"
-          title="Web Development"
-          text="Building websites that convert visitors into customers."
-          width="400"
-          height="400"
-        >
-        </ImageWithText>
-        <ImageWithText
-          image_placement="right"
-          src="/images/placeholder.png"
-          alt="TODO"
-          title="UI/UX Design"
-          text="Innovative design that captures attention and inspires action."
-          width="400"
-          height="400"
-        >
-        </ImageWithText>
+    <div>
+      <GradientHero
+        gradient="linear-gradient(45deg, #2e2b38, #5c3b3b, #2d5448)"
+        title="Our Services"
+      />
+      <ScrollingText
+        TextArray={[
+          "Shopify Ecommerce",
+          "UI/UX Design",
+          "Web Development",
+          "Consulting",
+          "3D Renders",
+          "Graphic Design",
+        ]}
+      />
+      <ImageWithText
+        image_placement="left"
+        src="/images/placeholder.png"
+        alt="TODO"
+        title="Web Development"
+        text="Building websites that convert visitors into customers."
+        width="400"
+        height="400"
+      ></ImageWithText>
+      <ImageWithText
+        image_placement="right"
+        src="/images/placeholder.png"
+        alt="TODO"
+        title="UI/UX Design"
+        text="Innovative design that captures attention and inspires action."
+        width="400"
+        height="400"
+      ></ImageWithText>
 
+      <ImageWithText
+        image_placement="left"
+        src="/images/placeholder.png"
+        alt="TODO"
+        title="3D Renders"
+        text="Enhance your brand's identity and message."
+        width="400"
+        height="400"
+      ></ImageWithText>
 
-        <ImageWithText
-          image_placement="left"
-          src="/images/placeholder.png"
-          alt="TODO"
-          title="3D Renders"
-          text="Enhance your brand's identity and message."
-          width="400"
-          height="400"
-        >
-        </ImageWithText>
+      <ImageWithText
+        image_placement="right"
+        src="/images/placeholder.png"
+        alt="TODO"
+        title="Consulting"
+        text="The help you need, when you need it."
+        width="400"
+        height="400"
+      ></ImageWithText>
 
-        <ImageWithText
-          image_placement="right"
-          src="/images/placeholder.png"
-          alt="TODO"
-          title="Consulting"
-          text="The help you need, when you need it."
-          width="400"
-          height="400"
-        >
-        </ImageWithText>
-
-        
-
-        <Footer></Footer>
-      </div>
+      <Footer></Footer>
+    </div>
   );
 };
 
