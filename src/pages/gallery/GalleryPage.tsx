d077d6359d2a13c3b4a31eff83a016c09f1b4fe5
frankev6 <frank.ev6@gmail.com>
2023-03-17 16:32:54 -0400

    gallery work

--- src/pages/gallery/GalleryPage.tsx
+++ src/pages/gallery/GalleryPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import gallery from "../../assets/gallery.json";
 import styles from "../../styles/GalleryPage.module.css";
 interface GroupProps {
@@ -9,15 +9,34 @@ interface GroupProps {
 const GalleryGroup = ({ title, images }: GroupProps) => {
   console.log(title, images);
 
+  const imageList = useRef<any>();
+
+  const scroll = (amount: number) => {
+    imageList.current.scrollBy({ left: amount, behaviour: "smooth" });
+  };
+
   return (
     <div className={styles.image_group}>
       <h2>{title}</h2>
-      {images.map((info, idx) => (
-        <div className={styles.image_list} key={idx}>
-          <img src={info.url} alt={info.caption} />
-          <p>{info.caption}</p>
+      <div style={{ width: "100%", position: "relative" }}>
+        <div
+          onClick={() => scroll(-200)}
+          className={`${styles.scroll_overlay} ${styles.left}`}
+        >
+          <img src="/images/arrow_right.svg" />
         </div>
-      ))}
+        <div onClick={() => scroll(200)} className={styles.scroll_overlay}>
+          <img src="/images/arrow_right.svg" />
+        </div>
+        <div ref={imageList} className={styles.image_list}>
+          {images.map((info, idx) => (
+            <div key={idx}>
+              <img draggable="false" src={info.url} alt={info.caption} />
+              <p>{info.caption}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
